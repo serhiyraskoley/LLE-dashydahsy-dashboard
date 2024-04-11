1032cf882933d63eac4db2d58dd8a2ad58a598e5
serhiyraskoley <r.serogka@gmail.com>
2024-04-11 15:52:09 +0300

    Clean up src/App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-
-import React, { useState, useEffect } from "react";
+import { Layout } from 'antd';
 import LeftSidebar from "./components/LeftSidebar";
-import { cardData } from './card.json';
-import { Layout, Typography, Card, Row, Col, Badge, Tag, Button } from 'antd';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { truncate } from 'lodash';
-const { Meta } = Card;
-const { Content } = Layout;
-library.add(fas);
+import CustomCards from "./components/Cards";
+
 const App = () => {
-  
-  
   return (
-    <LeftSidebar />
+      <Layout>
+        <LeftSidebar />
+          <CustomCards />
+      </Layout>
   );
 }
-
 
 export default App;
